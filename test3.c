fba13be87400b671373df8b8ecc9b303ced2b299
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-09-02 18:08:08 +0200

    ajouter test3.c

---
+++ test3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void){
+    int rayone  ;
+    
+    float p =3.14;
+    scanf("%d",&rayone);
+    float volume = (4.0f/3.0f)*p*rayone*rayone*rayone;
+    printf("%f",volume);
+    
+}
