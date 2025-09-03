141168d157b0a2b18e266c761a93a1591ae04455
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2025-09-03 11:45:58 +0200

    ajouter test5

---
+++ test5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    
+    printf("please enter the value of x :");
+    int x;
+    scanf("%d", &x);
+    int valeur=3*(x*x*x*x*x)*+2*(x*x*x*x)-5*(x*x*x-x*x)+7*x-6;
+    printf("%d", valeur);
+}
